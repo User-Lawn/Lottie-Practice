375b27d18647e5c6b36c25430598e6f3237b5027
이준영 <74142881+User-Lawn@users.noreply.github.com>
2022-06-10 14:40:32 +0900

    lottie

---
+++ Lottie/Lottie/LottieApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieApp.swift
+//  Lottie
+//
+//  Created by Jun.Mac on 2022/06/10.
+//
+
+import SwiftUI
+
+@main
+struct LottieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
